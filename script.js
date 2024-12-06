0243325811b56c7018b6cf6a40d8f4589bb80c33
Francesca-Lisi <francescalisi2143@gmail.com>
2024-12-06 12:58:17 +0100

    feat: fix overlay

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Recupero i dati necessari
 
 const endpoint = 'https://jsonplaceholder.typicode.com/photos?_limit=6'
-const image = document.getElementById('image');
+
 const note = document.querySelector('p')
 const row = document.querySelector('.row')
 const btnClose = document.querySelector('.btn-close')
@@ -21,28 +21,25 @@ axios.get(endpoint)
     const userCard = document.querySelectorAll('.user-card');
     console.log(userCard)
     imgOverlay = ''
+
     for (let card of userCard){
-        console.log(card)
-        card.addEventListener('click', (event) => {  
-          overlay.classList.remove('d-none')
-          let imageOver = `<img src="${event.target.src}" alt="img" class=" mx-auto d-block">`;
-          document.querySelector('.img-overlay').innerHTML = imageOver
+      console.log(card.children[1].src)
+      let image = card.children[1].src;
+      console.log(image)
+      card.addEventListener('click', (event) => {  
+
+        console.log(image)
+        overlay.classList.remove('d-none')
+        let imageOver = `<img src="${image}" alt="img" class=" mx-auto d-block">`;
+        document.querySelector('.img-overlay').innerHTML = imageOver
 
 
 
-        //   let imgOverlay = 
-        //   imgOverlay = ''
-        //   overlay.classList.remove('d-none')
-        //   imgOverlay.innerHTML = `<img src="${event.targ}" alt="img" class=" mx-auto d-block">`;
-        })
-      }
-
-    // console.log(response.data)
-    // response.data.forEach(element => {
-    //   const {id, url} = response.data;
-    //   console.log(userCard)
       
-    // })
+      })
+    }
+
+
   })
 
 
@@ -64,7 +61,7 @@ function printCard(card){
       `<div class="col col-12 col-md-6 col-lg-4">
         <div class="user-card">
           <img src="img/pin.svg" alt="pin">
-          <img id="image" src="${url}" alt="prova">
+          <img class="image" src="${url}" alt="prova">
           <p class="kalam-light">${capitalizedString}</p>
         </div>
       </div>`;
